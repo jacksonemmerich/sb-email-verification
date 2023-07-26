d7759274b8e6b3e226f416888b07852fb0e435be
Jackson Emmerich <jacksonemmerich@gmail.com>
2023-07-26 11:32:56 -0400

    saved in database and creation and created link of registration

--- src/main/java/com/jacksonemmerich/sbemailverificationdemo/registration/RegistrationController.java
+++ src/main/java/com/jacksonemmerich/sbemailverificationdemo/registration/RegistrationController.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class RegistrationController {
     private final UserService userService;
     private final ApplicationEventPublisher publisher;
     @PostMapping
-    public String registerUser(RegistrationRequest registrationRequest, final HttpServletRequest request){
+    public String registerUser(@RequestBody RegistrationRequest registrationRequest, final HttpServletRequest request){
         User user = userService.registerUser(registrationRequest);
         publisher.publishEvent(new RegistrationCompleteEvent(user,applicationUrl(request)));
         return "Sucess! Please, check your email for to complete your registration";
